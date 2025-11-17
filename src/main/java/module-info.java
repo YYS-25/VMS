c3d07a7a661c75f1b3dc423a5da3a75089cbb52b
Jdm_.x <99546768+js73484@users.noreply.github.com>
2025-11-17 08:38:45 +0400

    Your commit message here

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,6 +6,7 @@ module pkg.vms {
     requires com.dlsc.formsfx;
     requires org.kordamp.ikonli.javafx;
     requires org.kordamp.bootstrapfx.core;
+    requires java.sql;
 
     opens pkg.vms to javafx.fxml;
     exports pkg.vms;
